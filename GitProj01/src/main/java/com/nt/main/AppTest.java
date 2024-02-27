49c87374417589a54c4b7961705fd3df851e8dfa
HP <naveengolla555@gmail.com>
2024-02-27 13:06:10 +0530

    task1 for dev

--- GitProj01/src/main/java/com/nt/main/AppTest.java
+++ GitProj01/src/main/java/com/nt/main/AppTest.java
@@ -1,9 +1,12 @@
 package com.nt.main;
 
-import org.springframework.objenesis.instantiator.sun.MagicInstantiator;
+import com.nt.service.ArithemeticOperations;
 
 public class AppTest{
 	public static void main(String[] args) {
+		ArithemeticOperations ar=new ArithemeticOperations();
+		System.out.println("sum:"+ar.sum(200,100));
+		System.out.println("sub:"+ar.sub(200,100));
 		
 	}
 

cf9e327557df53e8ac2098eadde16fa732877310
HP <naveengolla555@gmail.com>
2024-03-02 16:04:39 +0530

    dev1 branch1 --task

--- GitProj01/src/main/java/com/nt/payment/PaymentOperationsService.java
+++ GitProj01/src/main/java/com/nt/payment/PaymentOperationsService.java
@@ -2,4 +2,11 @@ package com.nt.payment;
 
 public class PaymentOperationsService {
 
+	public String doCreditCardPayment() {
+		return " Credit card payment done";
+	}
+	public String doDebitCardPayment() {
+		return " Debit card payment done";
+	}
+	
 }
